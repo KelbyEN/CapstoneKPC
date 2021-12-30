c61289c66f3173207f7591c8271c654a998a4a11
Kelby Estes-Nelson <kelbyalese417@gmail.com>
2021-12-29 20:13:38 -0600

    updates to match testspa

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import html from "html-literal";
+
+export default () => html`
+  <footer>
+  <p>
+        <h3><b>Contact Us</b></h3>
+       <li>Email: KittyPinkCards@gmail.com</li>
+        <li>Number: 314-502-9952</li>
+        <li>Location: St. Louis Mo</li>
+
+    </p>
+    &copy; 2021 <a href="https://savvycoders.com/">KittyPinkCards</a>
+  </footer>
+`;
---
+++ store/Cart.js
@@ -0,0 +1,4 @@
+export default {
+  header: "My Cart Page",
+  view: "Cart"
+};

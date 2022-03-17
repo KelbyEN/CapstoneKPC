73d70b6a0cfbf675ccc080f2bb0fde671d022fb9
Kelby Estes-Nelson <kelbyalese417@gmail.com>
2022-03-17 18:24:42 -0500

    mack the poet

--- components/views/Books.js
+++ components/views/Books.js
@@ -33,8 +33,169 @@ export default () => html`
 
     <section class=poem>
       <h4><u>Poem of the Week</u></h4>
-    <p font size=18px>Want to feature your poem here?<br>
-  Email us and include your poem, name, and contact information</p>
+      <p class=week> Title: How you better than me? <br>
+      <br>
+
+Talking to you I get jealous <br>
+Your meekness maybe<br>
+Or how curiosity never<br>
+Killed your cat<br>
+See I was born in this<br>
+Still don’t understand this<br>
+Prolly never get this<br>
+But you<br>
+Something bout you different -<br>
+my brother wait<br>
+<br>
+<br>
+Dont say that<br>
+For you it’s not late<br>
+One thing you say it was<br>
+Think about this<br>
+Who got the message first though<br>
+it was you<br>
+Who has the power to control it in themself<br>
+It’s you<br>
+So what you doin with it ain’t on me<br>
+It’s on you<br>
+Jealousy is a sin<br>
+Sin Jehovah hates<br>
+You startin to act like Cain<br>
+Before you fall my brother wait <br>
+<br>
+<br>
+
+Man shut up<br>
+Don’t know my story<br>
+Nor my pain<br>
+So don’t start judging<br>
+Talking bout acting like Cain<br>
+So you Abel right<br>
+You over righteous piece of shit<br>
+Who are you<br>
+Telling me that’s on me<br>
+But tell me<br>
+Sitting in a room full of men<br>
+Talking all that knowledge<br>
+Them niggas that went to colleges<br>
+Tell me if you was me<br>
+What you gone believe<br>
+With the knowledge of only one thing<br>
+In knowledge I believe<br>
+Is some super power shit<br>
+That’s your only one up on me<br>
+Versatility and shit - <br>
+<br>
+<br>
+My brother wait<br>
+You don’t need anything<br>
+Except the truth<br>
+You got the book<br>
+You got the map<br>
+You got the key<br>
+Got the people<br>
+Got his hand<br>
+Enough support you<br>
+What else you need<br>
+Don’t listen to nobody but the one up above<br>
+Everybody else<br>
+Works against you<br>
+Don’t wanna see you succeed<br>
+Wanna hurt you<br>
+Pay attention to me<br>
+Have you ever read<br>
+1 Corinthians 15:33<br>
+I Don’t think so<br>
+open up your Bible<br>
+My brother can you read - <br>
+<br>
+<br>
+Listen here I was baptized off that scripture<br>
+I know it word from word<br>
+What you tryna test me<br>
+Jesus even loved criminals<br>
+Nigga fuck you<br>
+You don’t know nothing<br>
+Just as much as you think it’s good<br>
+It’s bad in that shit they ain’t perfect either<br>
+They hurt you worse<br>
+You’d think they’d be better<br>
+But I’ve been hurt the worst<br>
+By the people<br>
+And they say they living that shit<br>
+You think you safe huh<br>
+I can tell by the look on ya face,<br>
+Watch your back nigga<br>
+They got knives too<br>
+They move slick with the shit<br>
+Think you with your friends<br>
+Until they stab you in ya back<br>
+Rip your heart out<br>
+Drink your blood and suck your soul<br>
+Devour your body then suck the bone<br>
+As they watch you bleed out and shit<br>
+Swear you wouldn’t think it’s<br>
+So cold<br>
+So heartless<br>
+Worst part is<br>
+It be your closest<br>
+So you don’t say I "didn’t warn you"<br>
+Imma only say it one time my nigga<br>
+Don’t lose your focus<br>
+Since you think you better than me - <br>
+<br>
+<br>
+My brother wait<br>
+Yes they’re imperfect<br>
+Just like you say I’m over righteous<br>
+Look at you<br>
+You got a rafter<br>
+Yes they got twigs<br>
+Seems the only thing you captured<br>
+Feel the love<br>
+See the family<br>
+Feel his hand<br>
+let him mold you<br>
+Don’t be like Peter<br>
+Keep your eyes focused<br>
+He’ll get you out<br>
+Don’t you know this<br>
+Thinking cloudy<br>
+That’s from that smokin<br>
+Need to eat<br>
+Mind must be starving<br>
+Your brain been aching<br>
+The stress you hold<br>
+When all you need<br>
+Is to let him hold it<br>
+There’s a God with you<br>
+A God within you<br>
+I’m no better<br>
+I’ve been like you<br>
+I am you<br>
+So stop the nonsense<br>
+Regain your conscience<br>
+We have a purpose<br>
+It’s true<br>
+Three sides to the story<br>
+One truth there is<br>
+Only one of him<br>
+You have his number<br>
+You should call him<br>
+You got the book<br>
+But Tell everybody<br>
+Go find the truth<br>
+You can do the same<br>
+The things I’m saying<br>
+You already know this<br>
+But you think Im better than you<br>
+<br>
+<br>
+-Mack The Poet
+</p>
+
+    <h4 font size=18px>Want to feature your poem here?<br>
+  Email us and include your poem, name, and contact information</>
   </section>
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
   </section>

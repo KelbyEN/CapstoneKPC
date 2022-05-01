4768db27433b5f4740e175fe0acbcf93808a4d1e
Kelby Estes-Nelson <kelbyalese417@gmail.com>
2022-05-01 10:38:10 -0500

    feature poet

--- components/views/Feature.js
+++ components/views/Feature.js
@@ -1,5 +1,5 @@
 import html from "html-literal";
-import yaneebrinks from "../../assets/images/yaneebrinks.jpg";
+import ChrisKaiser from "../../assets/images/ChrisKaiser.jpg";
 import BlackAuthorsExpo from "../../assets/images/BlackAuthorsExpo.png";
 export default () => html`
 
@@ -15,41 +15,34 @@ export default () => html`
       <p class=week> <br>
       <br>
 
-      Love, pain, betrayal, and heartache<br>
-Just a few things that keep me awake<br>
-The thought that I'm losing all my fights<br>
-Is the very thing that keeps me up at night<br>
-No it's not the fact that I experienced pain<br>
-It's the fact that I feel like I didn't gain<br>
-It's not the fact that I fell in love again<br>
-It's the thought that I'm so broken that this love will also soon end<br>
-It's not the many times that I've experienced heartache<br>
-It's that I didn't expect to see trusted faces that caused my heart to break<br>
-And yeah we all experience betrayal at some point in time<br>
-But how do I stop it from constantly replaying in my mind?<br>
-I tell myself to keep going<br>
-I try to keep the pain from showing<br>
-But when the weight of the world is on your shoulders<br>
-No one can tell you that it's not over<br>
-Because when the only thing that makes you smile<br>
-Is the pure and innocence pouring into you from your child<br>
-It builds you up so high then tears you down<br>
-Man I hate that they want to see me smile but understand why I frown<br>
-I hate crying when they're around<br>
-I hate that they are the ones fixing my crooked crown<br>
-I hate that I'm so unsure of myself<br>
-Yet I'm always finding someone else's self wealth <br>
-I keep telling myself to close up shop an step away from the world<br>
-But Yanee don't want y'all to forget about your girl<br>
-So I pour my heart out into these words and write another day<br>
-Hoping that one day these words will take the pain away<br>
+      Title:Beyond A Crush<br>
+      <br>
+Never felt this way before<br>
+Feelings bursting out from the core of my heart<br>
+This tells the story of where you and I start<br>
+Many days I sit waiting by my phone<br>
+For our daily talks<br>
+Hoping that we hold hands,<br>
+As we walk through the park<br>
 <br>
+As I scroll through your pictures,<br>
+I remember why I dig you<br>
+I envision big things with you<br>
+Like dreams to share great joy with you,<br>
+But you left me with no choice<br>
+I have to confess that I’m in love with you<br>
 <br>
-
-Love, Hurt, & Heartache © 2022 by Yanee Brinks is licensed under CC BY-NC-ND 4.0<br>
+Be each other’s strength,<br>
+If one of us falls behind<br>
+Lift each other to stand strong aligned<br>
+Together we can create a whole new bloodline<br>
 <br>
-<br>
--Yanee Brinks
+Let those past pains perish<br>
+So we can be what they didn’t accomplish,<br>
+A winning hand<br>
+A royal flush<br>
+A four of a kind<br>
+Allowing us to be something beyond a crush<br>
 </p>
 
 <button class="btn btn-like">
@@ -67,13 +60,8 @@ Love, Hurt, & Heartache © 2022 by Yanee Brinks is licensed under CC BY-NC-ND 4.
     </span>
 </button>
 
-<h4>For booking Yanee Brinks:<br>
-<img src=${yaneebrinks} alt="anxiety" width="300x300" /><br />
-<ul>www.YaneeBrinks.net</ul>
-<ul>Amazon.com/author/yaneebrinks</ul>
-<ul>www.Savageauthors.com</ul>
-<ul>www.Facebook.com/AuthorYanee</ul>
-
+<h4>For booking Chris:<br>
+<img src=${ChrisKaiser} alt="anxiety" width="400x400" /><br />
 </h4>
 
     <h4 font size=18px>Want to feature your poem here?<br>

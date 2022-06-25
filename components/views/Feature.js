5d69ef7de95ca1061991ef76532b8b43e1fe5c01
Kelby Estes-Nelson <kelbyalese417@gmail.com>
2022-06-25 08:27:43 -0500

    feature

--- components/views/Feature.js
+++ components/views/Feature.js
@@ -1,14 +1,14 @@
 import html from "html-literal";
-import ChrisKaiser from "../../assets/images/ChrisKaiser.jpg";
+import Kodaq from "../../assets/images/Kodaq.png";
 import BlackAuthorsExpo from "../../assets/images/BlackAuthorsExpo.png";
 import TheCouplesPlaydate from "../../assets/images/TheCouplesPlaydate.jpg";
 export default () => html`
 
-<section class=events>
+<!-- <section class=events>
 <h1>Upcoming Events</h1>
 <img src=${BlackAuthorsExpo} alt="anxiety" width="400x400" /><br />
 <p>KittyPinkCards will have Mothers's Day baskets and cards for sale as well as access to purchase PoeticPhotos</p>
-</section>
+</section> -->
 
 
 <section class=poem>
@@ -16,42 +16,37 @@ export default () => html`
       <p class=week> <br>
       <br>
 
-      Title:Beyond A Crush<br>
+      Title:Black Man<br>
       <br>
-Never felt this way before<br>
-Feelings bursting out from the core of my heart<br>
-This tells the story of where you and I start<br>
-Many days I sit waiting by my phone<br>
-For our daily talks<br>
-Hoping that we hold hands,<br>
-As we walk through the park<br>
-<br>
-As I scroll through your pictures,<br>
-I remember why I dig you<br>
-I envision big things with you<br>
-Like dreams to share great joy with you,<br>
-But you left me with no choice<br>
-I have to confess that I’m in love with you<br>
-<br>
-Be each other’s strength,<br>
-If one of us falls behind<br>
-Lift each other to stand strong aligned<br>
-Together we can create a whole new bloodline<br>
-<br>
-Let those past pains perish<br>
-So we can be what they didn’t accomplish,<br>
-A winning hand<br>
-A royal flush<br>
-A four of a kind<br>
-Allowing us to be something beyond a crush<br>
+
+
+I see you Black Man,<br>
+all on the internet shaming women, I see you black man<br>
+in every picture throwing gang signs, I see you Black Man,<br>
+all in the streets killing each other, I see you Black Man,<br>
+leaving your kids to fend for themselves, I see you black man<br>
+Oh you to good to work, I see you black man<br>
+we might as well kill you so we don’t see you black man,<br>
+No, you want to kill me because you are afraid of this black man,<br>
+all on the internet loving our black women as a black man,<br>
+in every picture with our degrees as a black man,<br>
+all in the streets rebuilding our community as a black man,<br>
+showing up for our kids and cheering them on as a black man,<br>
+taking over corporate America and having our businesses as a black man,<br>
+you are afraid of that black man so u try to paint a negative picture for the world to see of a black man,<br>
+we built this country and youre afraid of my power as a black man,<br>
+everybody loves us and our culture so you envy the black man,<br>
+to my black men—protect our women and keep raising our kids as a black man,<br>
+walk tall and head high as a proud black man,<br>
+Because theres no greater feeling than being a beautiful Black Man<br>
 </p>
 
-<h4>For booking Chris:<br>
-<img src=${ChrisKaiser} alt="anxiety" width="450x450" /><br />
-</h4>
+<p>Poem by: Kodaq <br> Kodaq is an entrepreneur, curator, creator and brand developer who enjoys writing in his free time. Find him on Instagram by clicking the picture below
+<br>
+<a href="https://instagram.com/kodaq314?igshid=YmMyMTA2M2Y=" target="popup"><img src=${Kodaq} alt="anxiety" width="450x450" /><br /></a>
+</p>
 <!--
 <h4>If you liked this poem let the Poet know by commenting below!</h4>
-
 <div id="comment"><input type="text" id="comment-box" placeholder="Enter comment">
     <button id="post">Post</button>
     <ul id="unordered">
@@ -66,12 +61,16 @@ Allowing us to be something beyond a crush<br>
 
   <h4>Past Events</h4>
 
-<p><h1>The Couples Playdate<br>
+<p>The Couples Playdate<br>
 This was so much fun! Thecouplesplaydate.com for more info
 <br>
 <ul><a href="https://drive.google.com/drive/u/1/folders/16M64luy1Rw8EvI_s1tk-x-HFDnZAheQ7" target="popup">
     <img src=${TheCouplesPlaydate} alt="The Couples Playdate Event" width=400x400/>
-   </ul></h4><br>Click for event pictures<br></a>
+   </ul></p><br>Click for event pictures<br></a>
+   <p>Black Authors Expo<br>
+  There were so many great Authors in the room<br>
+<u;><a href="https://drive.google.com/drive/folders/1eGr0A6-ZMmdMfJ1yRHw4XCUU5Pfoh3JI?usp=sharing" target="popup"></u></h1>
+<img src=${BlackAuthorsExpo} alt="Black Authors Expo" width=400x400/><br>Click for event pictures<br></a>
    </p>
   </section>
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
